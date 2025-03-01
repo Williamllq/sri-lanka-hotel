1d4452f4a20835db65192d6d98d56e4920934498
Williamllq <william.llq.2018@gmail.com>
2025-03-01 21:58:21 +0800

    将服务器端口从3000更改为3001，避免端口冲突

--- server/index.js
+++ server/index.js
@@ -39,7 +39,7 @@ app.post('/payment-success', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 }); 
